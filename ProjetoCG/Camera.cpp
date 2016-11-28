991cc41970e39bc041b569c27fb494d76f8f99af
vicenteavel <vicente_flip@hotmail.com>
2016-11-27 23:43:19 -0300

    Update Camera.cpp

--- ProjetoCG/Camera.cpp
+++ ProjetoCG/Camera.cpp
@@ -10,6 +10,9 @@ using namespace std;
 namespace sowi{
 
 	Camera::Camera(){
+		move = true;
+		futposx = 0.0f;
+		futposz = 0.0f;
 		posx = 0.0f;
 		posy = 3.0f;
 		posz = 0.0f;
@@ -25,14 +28,15 @@ namespace sowi{
 		tras = false;
 		sensmove = 0.08f;
 		timer = 0;
-		mousePosx = 0.0f;
+		mousePosx = 180.0f;
 		mousePosy = 95.0f;
 		width = 800;
 		height = 600;
 		player = {0.5f, 0.5f, -0.5f, -0.5f};
+		futplayer = {0.5f, 0.5f, -0.5f, -0.5f};
 	}
 	Camera::~Camera(){
-		
+
 	}
 	void Camera::frenteStart(){
 		frente = true;
@@ -58,7 +62,7 @@ namespace sowi{
 	void Camera::trasStop(){
 		tras = false;
 	}
-	
+
 	void Camera::keyPressed(unsigned char key, int x, int y){
 		if(key == 'w'){
 			frenteStart();
@@ -73,7 +77,7 @@ namespace sowi{
 			direitaStart();
 		}
 	}
-	
+
 	void Camera::keyUp(unsigned char key, int x, int y){
 		if(key == 'w'){
 			frenteStop();
@@ -88,7 +92,7 @@ namespace sowi{
 			direitaStop();
 		}
 	}
-	
+
 	void Camera::lookerCamera(int x, int y){
 		mousePosx -= (x - (width/2))*0.1f;
 		mousePosy += (y - (height/2))*0.1f;
@@ -99,45 +103,76 @@ namespace sowi{
 			mousePosy = 0.0000001f;
 		}
 	}
-	
+
 	void Camera::mover(){
-		
+
 		int atualtimer = glutGet(GLUT_ELAPSED_TIME);
 		int intervalo = atualtimer - timer;
-		
+
 		lookx = sin(mousePosy*PI/180)*sin(mousePosx*PI/180);
 		looky = cos(mousePosy*PI/180);
 		lookz = sin(mousePosy*PI/180)*cos(mousePosx*PI/180);
-		
+
 		if(frente){
-			posx += intervalo * lookx / 10*sensmove;
-			posz += intervalo * lookz / 10*sensmove;
+			futposx += intervalo * lookx / 10*sensmove;
+			futposz += intervalo * lookz / 10*sensmove;
 		}
 		if(esquerda){
-			posx += intervalo * sin((mousePosx+90)*PI/180) / 10*sensmove;
-			posz += intervalo * cos((mousePosx+90)*PI/180) / 10*sensmove;
+			futposx += intervalo * sin((mousePosx+90)*PI/180) / 10*sensmove;
+			futposz += intervalo * cos((mousePosx+90)*PI/180) / 10*sensmove;
 		}
 		if(tras){
-			posx -= intervalo * lookx / 10*sensmove;
-			posz -= intervalo * lookz / 10*sensmove;
+			futposx -= intervalo * lookx / 10*sensmove;
+			futposz -= intervalo * lookz / 10*sensmove;
 		}
 		if(direita){
-			posx -= intervalo * sin((mousePosx+90)*PI/180) / 10*sensmove;
-			posz -= intervalo * cos((mousePosx+90)*PI/180) / 10*sensmove;
+			futposx -= intervalo * sin((mousePosx+90)*PI/180) / 10*sensmove;
+			futposz -= intervalo * cos((mousePosx+90)*PI/180) / 10*sensmove;
 		}
-		
+
+		timer = atualtimer;
+		glutWarpPointer(width/2,height/2);
+
+		futplayer.MaxX = futposx + 0.5f;
+		futplayer.MaxZ = futposz + 0.5f;
+		futplayer.MinX = futposx - 0.5f;
+		futplayer.MinZ = futposz - 0.5f;
+
 		camx = posx + lookx;
 		camy = posy + looky;
 		camz = posz + lookz;
-		
-		timer = atualtimer;
-		glutWarpPointer(width/2,height/2);
 	}
-	
+
 	void Camera::attAABB(){
-		player.MaxX = posx + 0.5f;
-		player.MaxZ = posz + 0.5f;
-		player.MinX = posx - 0.5f;
-		player.MinZ = posz - 0.5f;
+		if(move){
+			posx = futposx;
+			posz = futposz;
+			player.MaxX = posx + 0.5f;
+			player.MaxZ = posz + 0.5f;
+			player.MinX = posx - 0.5f;
+			player.MinZ = posz - 0.5f;
+
+			futplayer.MaxX = player.MaxX;
+			futplayer.MaxZ = player.MaxZ;
+			futplayer.MinX = player.MinX;
+			futplayer.MinZ = player.MinZ;
+
+			camx = posx + lookx;
+			camy = posy + looky;
+			camz = posz + lookz;
+		} else {
+			futposx = posx;
+			futposz = posz;
+			futplayer.MaxX = futposx + 0.5f;
+			futplayer.MaxZ = futposz + 0.5f;
+			futplayer.MinX = futposx - 0.5f;
+			futplayer.MinZ = futposz - 0.5f;
+
+			camx = posx + lookx;
+			camy = posy + looky;
+			camz = posz + lookz;
+
+			move = true;
+		}
 	}
 }
